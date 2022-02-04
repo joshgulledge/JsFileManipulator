d6f63e9ee096932cb1a0efde8d180556faf7a07a
Josh Gulledge <joshcalebgulledge@gmail.com>
2022-02-03 22:59:48 -0600

    writes new letter to txt file, still needs formatting

--- FileChanger.js
+++ FileChanger.js
@@ -1,19 +1,30 @@
 const fs = require("fs");
 const readNames = require('./CsvReader');
+const readLetter = require('./LetterTxtReader');
 
 // get name and address data
 let people = readNames('/Users/joshgulledge/Documents/Field Service/Territory.csv');
-console.log(people);
-
-// get letter data
-
 
 // combine each name and address to each letter
+const joiner = function (peopleArr) {
+  // for get letter array
+  let letterArr = readLetter("/Users/joshgulledge/Documents/Field Service/TrialLetter.txt");
+  
+  // write name to the letter
+  Object.values(people).forEach((person, index) => {
+    let newLetter = [...letterArr];
+    let newLine = newLetter[1].split(' ');
+    newLine[1] = person.name + ",";
+    newLetter[1] = newLine.join(' ');
+    // create new file with each new letter
+    fs.writeFile(`file${index}.txt`, newLetter.join(' '), function(err) {
+      if (err) {
+        console.log(err);
+        return err;
+      }
+    })
+  });
 
-// Object.values(people).forEach((person, index) => {
-//   fs.writeFile(`file${index}.txt`, `${person.name}`, function(err) {
-//      if (err) {
-//         return console.error(err);
-//      }
-//   });
-// });
+};
+
+joiner(people);
--- LetterTxtReader.js
+++ LetterTxtReader.js
@@ -3,17 +3,22 @@ const fs = require('fs');
 //"/Users/joshgulledge/Documents/Field Service/Gov Letter 2"
 const readLetter = function (filePath) {
   let data = fs.readFileSync(filePath, 'utf8'); 
-  var dataArray = data.split(/\r?\n/);
-  let line;
+  // var dataArray = data.split(/\r?\n/);
+  return data.split(/\r?\n/);
+  // console.log("This is the data array 🥃 ");
+  // console.log(dataArray);
+  // let line;
   
-  dataArray.forEach((row, index) => {
-    line = row.split(',');
-    console.log(Array.from(line[0]));
-  });
+  // dataArray.forEach((row, index) => {
+  //   console.log("this is the row 🦋");
+  //   console.log(row);
+  //   line = row.split(',');
+
+  //   console.log("this is the line 🎉");
+  //   console.log(line);
+  // });
   
-  console.log(line);
-  return line;
 };
 
-console.log(readLetter("/Users/joshgulledge/Documents/Field Service/TrialLetter.txt"));
-//module.exports = readNames;
+console.log("outside", readLetter("/Users/joshgulledge/Documents/Field Service/TrialLetter.txt"));
+module.exports = readLetter;
